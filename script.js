62f914145d54c8d2a16761e80c71db37e3d7c1fe
ethan-walker <walkere@stpaulsschool.org.uk>
2025-02-21 20:17:19 +0000

    bare bones code

---
+++ script.js
@@ -0,0 +1,65 @@
+const inputElement = document.getElementById("data-input");
+
+function stringToDict(str) {
+   rows = str.split("\r\n").map(row => row.split(","));
+   header = rows[0].map(item => item.toLowerCase());
+   rows = rows.slice(1);
+   grid = rows.map(row => {
+      let dict = {};
+      row.forEach((item, index) => {
+         dict[header[index]] = item;
+      })
+      return dict;
+   });
+   return grid.slice(0, -1);
+}
+const arrAvg = (arr) => arr.reduce((acc, curr) => parseFloat(acc) + parseFloat(curr)) / arr.length;
+
+const mostFrequent = arr =>
+   Object.entries(
+     arr.reduce((a, v) => {
+       a[v] = (a[v] ?? 0) + 1;
+       return a;
+     }, {})
+   ).reduce((a, v) => (v[1] >= a[1] ? v : a), [null, 0])[0];
+
+inputElement.onchange = handleZip;
+
+// function() {
+//    var zip = new JSZip();
+//    zip.loadAsync( this.files[0] /* = file blob */)
+//       .then(function(zip) {
+//           console.log(zip);
+//          //  const reader = new FileReader();
+//          //  reader.readAsText(diary);
+      
+// };
+async function loadFileContents(zip, filename) {
+   return zip.file(filename).async("string").then(data => stringToDict(data));
+}
+function handleZip() {
+   var zip = new JSZip();
+   zip.loadAsync( this.files[0] /* = file blob */)
+      .then(function(zip) {
+         loadFileContents(zip, "diary.csv").then(handleDiary);
+
+         loadFileContents(zip, "watched.csv").then(handleWatched);
+      }, function() {alert("Not a valid zip file")});
+}
+
+function handleDiary(data) {
+   ratings = data.map(row => row.rating);
+   avgRating = arrAvg(ratings).toFixed(2);
+   document.querySelector(".rating").textContent = "Average Rating: " + avgRating;
+
+   modeRating = mostFrequent(ratings);
+   document.querySelector(".mode").textContent = "Most Common Rating: " + modeRating;
+
+   years = data.map(row => row.year);
+   avgYear = Math.round(arrAvg(years));
+   document.querySelector(".release-year").textContent = "Average Release Year: " + avgYear;
+}
+
+function handleWatched(data) {
+   
+}
